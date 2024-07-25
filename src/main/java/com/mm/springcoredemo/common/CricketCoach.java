a4c08f7c243d63f5283327599d10e0c81c61bc72
Miguel Montagner <dev@miguelmontagner.com>
2024-07-25 05:28:12 +0100

    Using PostConstruct and PreDestroy

--- src/main/java/com/mm/springcoredemo/common/CricketCoach.java
+++ src/main/java/com/mm/springcoredemo/common/CricketCoach.java
@@ -1,17 +1,28 @@
 package com.mm.springcoredemo.common;
 
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class CricketCoach implements Coach{
     public CricketCoach() {
         System.out.println(STR."In Constructor: \{getClass().getSimpleName()}");
     }
 
+    //init method
+    @PostConstruct
+    public void startTheEngines() {
+        System.out.println(STR."in PostConstruct in \{getClass().getSimpleName()}");
+    }
+    //destroy method
+    @PreDestroy
+    public void lightsOut() {
+        System.out.println(STR."in PreDestroy in \{getClass().getSimpleName()}");
+    }
     @Override
     public String getDailyWorkout() {
         return "Practice fast bowling for 15 minutes!!!!!";
